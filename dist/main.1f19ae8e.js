b2969411d901bc095ca93cded8a734c9d1a60b0b
Eugene Koryabkin <eugenewalknut@gmail.com>
2020-08-03 05:48:16 +0300

    build

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -3024,7 +3024,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1879" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6913" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
